c47b789de6100adb491061468eb2ba9a78afa66e
Veronica Xue <johnnysangel@163.com>
2016-06-02 11:01:12 +0800

    Adapt to files of all names

--- src/Merger.java
+++ src/Merger.java
@@ -2,8 +2,10 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -22,20 +24,30 @@ public class Merger {
 		
 		//
 		Integer index = 1;
-		Integer newIndex = 147;
-		while(true){
+		BufferedReader br = new BufferedReader(new FileReader("/Users/veronica/Desktop/integration/unhandled/stamp"));
+	    String stamp = br .readLine();
+	    
+		Integer newIndex = Integer.valueOf(stamp);
 
-			String infoPath = "/Users/veronica/Desktop/new44/noel.icr.exp" + index.toString() + ".info";
-			File f = new File(infoPath);
-			if(!f.exists()){
-				break;
-			}
+
+			List<String> fileList = new ArrayList<>();
+			
+			Files.walk(Paths.get("/Users/veronica/Desktop/integration/unhandled")).forEach(filePath -> {
+				
+				if(filePath.toString().contains("info")){
+					fileList.add(filePath.toString());
+				}
+			});
+			System.out.println(fileList);
+
 			int count = 0;
-			List<String> pictures = new ArrayList<>();
+
 			List<String> characterList = new ArrayList<>();
-			for(Integer i = index; i < index + 20; i++){
-				infoPath = "/Users/veronica/Desktop/new44/noel.icr.exp" + i.toString() + ".info";
-				f = new File(infoPath);
+			List<String> pictures = new ArrayList<>();
+			
+			for(String infoPath: fileList){
+
+				/*File f = new File(infoPath);
 				if(!f.exists()){
 					BufferedImage img1 = ImageIO.read(new File(pictures.get(0)));
 					String characters1 = characterList.get(0);
@@ -49,14 +61,16 @@ public class Merger {
 						characters1 = mergedCharacters;
 						img1 = merged;
 					}
-					boolean success = ImageIO.write(merged, "tif", new File("/Users/veronica/Desktop/new3/noel.icr.exp" + newIndex.toString() + ".tif"));
-					writeToFile(mergedCharacters, "/Users/veronica/Desktop/new3/noel.icr.exp" + newIndex.toString() + ".info");
+					boolean success = ImageIO.write(merged, "tif", new File("/Users/veronica/Desktop/integration/merged/noel.icr.exp" + newIndex.toString() + ".tif"));
+					writeToFile(mergedCharacters, "/Users/veronica/Desktop/integration/merged/noel.icr.exp" + newIndex.toString() + ".info");
 					
 		            System.out.println("saved success? "+success);
 		            return;
-				}
+				}*/
 				String characters = getCharactersFromInfoFile(infoPath);
 				count += characters.length();
+				pictures.add(infoPath.substring(0, infoPath.length()-4) + "tif");
+				characterList.add(characters);
 				if(count >= 40){
 					//merge the pictures and .info
 					BufferedImage img1 = ImageIO.read(new File(pictures.get(0)));
@@ -71,25 +85,71 @@ public class Merger {
 						characters1 = mergedCharacters;
 						img1 = merged;
 					}
-					boolean success = ImageIO.write(merged, "tif", new File("/Users/veronica/Desktop/new3/noel.icr.exp" + newIndex.toString() + ".tif"));
-					writeToFile(mergedCharacters, "/Users/veronica/Desktop/new3/noel.icr.exp" + newIndex.toString() + ".info");
+					boolean success = ImageIO.write(merged, "tif", new File("/Users/veronica/Desktop/integration/merged/noel.icr.exp" + newIndex.toString() + ".tif"));
+					writeToFile(mergedCharacters, "/Users/veronica/Desktop/integration/merged/noel.icr.exp" + newIndex.toString() + ".info");
 		            System.out.println("saved success? "+success);
 		            newIndex ++;
-					index = i;
-					break;
-				}else{
- 
-					pictures.add(infoPath.substring(0, infoPath.length()-4) + "tif");
-					characterList.add(characters);
+
+					count = 0;
+
+				    characterList = new ArrayList<>();
+					pictures = new ArrayList<>();
 				}
 			}
+			
+			if(!characterList.isEmpty()){
+				BufferedImage img1 = ImageIO.read(new File(pictures.get(0)));
+				String characters1 = characterList.get(0);
+				BufferedImage merged = img1;
+				String mergedCharacters = characters1;
+				for(int j = 1; j < pictures.size(); j++){
+					BufferedImage img2 = ImageIO.read(new File(pictures.get(j)));
+					merged = joinBufferedImage(img1, img2);
+					String characters2 = characterList.get(j);
+					mergedCharacters = characters1 + characters2;
+					characters1 = mergedCharacters;
+					img1 = merged;
+				}
+				boolean success = ImageIO.write(merged, "tif", new File("/Users/veronica/Desktop/integration/merged/noel.icr.exp" + newIndex.toString() + ".tif"));
+				writeToFile(mergedCharacters, "/Users/veronica/Desktop/integration/merged/noel.icr.exp" + newIndex.toString() + ".info");
+	            System.out.println("saved success? "+success);
+	            newIndex ++;
+			}
 
+
+
+			File file = new File("/Users/veronica/Desktop/integration/unhandled/stamp");
+
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(newIndex.toString());
+			bw.close();
 			
 			
-		}
+			moveFilesToHandled(fileList);
+			
+
        
     }
 
+	
+	public static void moveFilesToHandled(List<String> fileList){
+		for(String infoPath: fileList){
+			String imgPath = infoPath.substring(0, infoPath.length()-4) + "tif";
+			File infoFile = new File(infoPath);
+			File imgFile = new File(imgPath);
+			String[] splits = imgPath.substring(0, infoPath.length()-4).split("/");
+			System.out.println(splits);
+			String fileName = splits[splits.length-1];
+			infoFile.renameTo(new File("/Users/veronica/Desktop/integration/handled/" + fileName + "info"));
+			imgFile.renameTo(new File("/Users/veronica/Desktop/integration/handled/" + fileName + "tif"));
+		}
+	}
 	public static void writeToFile(String content, String path) throws IOException{
 		List<String> lines = new ArrayList<>();
 		lines.add(content);
